973ed4633450b3108bcb6f932cd0befae90d7b78
patricioyaneza <pa.yaneza@outlook.cl>
2025-12-23 18:35:14 -0300

    update

---
+++ Modulo 2/Ejercicio1/src/Principal.java
@@ -0,0 +1,22 @@
+
+// Nombre de la clase y el archivo tienen que tener el mismo nombre
+// Cada palabra la 1ra letra en mayuscula
+public class Principal {
+	static int  variableGlobal = 1000;
+	// entry point ( punto de arranque )
+	// método
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola Mundo !!!");
+		int valorProductoBonita  =0;
+		int a1 = 0;
+		
+		System.out.print("" + a1 + " " + variableGlobal);
+		
+		a1 = 15;		
+		
+		int a2 = 100;
+		a2 = 1000;
+	}
+
+}
