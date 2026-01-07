4d5199618a794177bd569ff83d1e802dc5d87c65
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-06 21:59:13 -0300

    UPDATE

--- Modulo 2/Ejercicio6/src/Principal.java
+++ Modulo 2/Ejercicio6/src/Principal.java
@@ -24,6 +24,43 @@ public class Principal {
 			System.out.println("El número es positivo. 2");
 			System.out.println("======================");
 		}
+
+		System.out.println("Evaluar con if else if ");
+		// numero es +, - y neutro (0)
+		if(valor < 0)
+		{
+			System.out.println("El número es negativo -");			
+		}
+		else if(valor == 0)
+		{
+			System.out.println("El número es cero");
+		}
+		else 
+		{
+			System.out.println("El número es Positivo +");
+		}
+		
+// Ejercicio 2: ver Principal2
+		
+		
+		boolean existe = false;
+		
+		if(existe)
+			System.out.println("======> Si existe.");
+		else
+			System.out.println("======> No existe.");
+		
+		
+		int valor2 = 10;
+		boolean esMayorDeEdad = false;
+		String resultado = "";
+		
+		esMayorDeEdad = valor2 >= 18 ? true : false;
+		resultado = valor2 >= 18? "Si": "No";
+		System.out.println("Es mayor de edad: " + esMayorDeEdad);
+		System.out.println("Es mayor de edad: " + resultado);
+		System.out.println("Es mayor de edad: " + (valor2 >= 18 ?"Si":"No"));
+		
 		
 		System.out.println("=======  Fin de la aplicación  ========");
 	}
---
+++ Modulo 2/Ejercicio6/src/Principal2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Ejercicio, preguntar al usuario la edad y definir si es 
+		// niño 12, adolescente 17, adulto 64 o 3ra edad
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.print("Ingrese la edad: ");
+		int edad = teclado.nextInt();
+
+		if (edad <= 12) {
+			System.out.println("Niño");
+		} else if (edad >= 13 && edad <= 17) {
+			System.out.println("Adolescente");
+		} else if (edad >= 18 && edad <= 64) {
+			System.out.println("Adulto");
+		} else {
+			System.out.println("Tercera edad");
+		}
+
+		if (edad <= 12) {
+			System.out.println("Es niño");
+		} else if (edad <= 17) {
+			System.out.println("Es adolescente");
+		} else if (edad <= 64) {
+			System.out.println("Es adulto");
+		} else {
+			System.out.println("Es de la tercera edad");
+		}
+		
+	}
+
+}
