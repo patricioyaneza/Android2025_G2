0ef5fb289afd93d98e8c1cb1f3efc572fdfda9d0
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-08 19:42:52 -0300

    update

--- Modulo 2/Ejercicio6/src/Principal6.java
+++ Modulo 2/Ejercicio6/src/Principal6.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Principal6 {
 
@@ -11,6 +12,52 @@ public class Principal6 {
 		Mostrar error si el numero no es del 1 al 12.
 
 		*/
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Ingrese un número de mes (1-12): ");
+		int numeroMes = entrada.nextInt();
+
+		switch (numeroMes) {
+		case 1: 
+			System.out.println("Enero"); 
+			break;
+		case 2: 
+			System.out.println("Febrero"); 
+			break;
+		case 3: 
+			System.out.println("Marzo"); 
+			break;
+		case 4: 
+			System.out.println("Abril"); 
+			break;
+		case 5: 
+			System.out.println("Mayo"); 
+			break;
+		case 6: 
+			System.out.println("Junio"); 
+			break;
+		case 7: 
+			System.out.println("Julio"); 
+			break;
+		case 8: 
+			System.out.println("Agosto"); 
+			break;
+		case 9: 
+			System.out.println("Septiembre"); 
+			break;
+		case 10: 
+			System.out.println("Octubre"); 
+			break;
+		case 11: 
+			System.out.println("Noviembre"); 
+			break;
+		case 12: 
+			System.out.println("Diciembre"); 
+			break;
+		default:
+			System.out.println("Error: El número debe ser entre 1 y 12.");
+			break;
+		}
+		
 	}
 
 }
---
+++ Modulo 2/Ejercicio6/src/Principal7.java
@@ -0,0 +1,20 @@
+
+public class Principal7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		EJERICICIO 7:
+
+		Para una empresa de domotica, se requiere encender artefactos 
+		disponibles en la empresa.
+		Los dispositivos disponibles son: OF1, OF2, OF3, 
+		MAQUINA1, MAQUINA2 y VENTILADORES
+		La aplicacion debe solicitar el nombre del dispositivo o su numero 
+		y "encender" el dispositivo solicitado.
+
+
+		*/
+	}
+
+}
