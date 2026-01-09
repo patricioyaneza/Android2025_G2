ec61fecfe2c92f9638fd8682c2293b18229dd60e
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-09 19:57:55 -0300

    Update Principal2.java

--- Modulo 2/Ejercicio7/src/Principal2.java
+++ Modulo 2/Ejercicio7/src/Principal2.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Principal2 {
 
@@ -9,8 +10,29 @@ public class Principal2 {
 		 * o sea, sin guion ni digito verificador
 		 * 20100300-5  => 20100300
 		 *  9200350-7  =>  9200350
+		 *153100200-1  =>153100200
+		 *  
+		 * 2.- Aparte, mostrar el digito verificador
+		 *  9200350-7  => 7
 		 *  
 		 */
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Ingrese su rut: ");
+		String rut = scanner.nextLine();
+
+		if (rut.contains("-")) {
+			String numeroRut = rut.substring(0, rut.indexOf("-"));
+			System.out.println("rut: " + numeroRut);
+		} else {
+			System.out.println("rut incorrecto.");
+		}
+		
+		String digitoVerificador = rut.substring(rut.indexOf("-") + 1);
+		System.out.println("digito verificador: " + digitoVerificador);
+		
+		
+		
 	}
 
 }
