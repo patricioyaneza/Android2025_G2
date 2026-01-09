106184234bd143a23ceb2bb6619663206ed52e89
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-09 19:12:13 -0300

    update

---
+++ Modulo 2/Ejercicio7/src/Principal2.java
@@ -0,0 +1,16 @@
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Ejercicio 10:
+		 * 
+		 * 1.- Ingrese un rut con digito verificador y muestre solo el numero
+		 * o sea, sin guion ni digito verificador
+		 * 20100300-5  => 20100300
+		 *  9200350-7  =>  9200350
+		 *  
+		 */
+	}
+
+}
