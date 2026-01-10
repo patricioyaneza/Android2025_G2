4ab39d20ea37fe3220d7d372691a5171279dbde8
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-09 22:28:55 -0300

    update

---
+++ Modulo 2/Ejercicio9/src/Principal3.java
@@ -0,0 +1,14 @@
+
+public class Principal3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+ * Crear una cuanta regresiva desde el valor que indique el usuario
+ * utilizar for para mostrar la cuenta regresiva
+ * 
+ * 
+ */
+	}
+
+}
