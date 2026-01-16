42ec9c5488181ab06642779fc97861d2f69d0926
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-15 21:38:29 -0300

    update

---
+++ Modulo 2/Ejercicio10/src/Automovil.java
@@ -0,0 +1,23 @@
+
+public class Automovil {
+
+	// atributos
+	// tipo de dato nombre    // AL7050  JGGG90
+	String patente;
+	String marca;
+	String modelo;
+/*	
+	String color;
+	int precioCosto;
+	int precioVenta;
+*/
+	// metodo constructor
+	public Automovil(String patente, String marca, String modelo)
+	{
+		this.patente = patente;
+		this.marca = marca;
+		this.modelo = modelo;
+	}
+	
+	// metodos propios
+}
