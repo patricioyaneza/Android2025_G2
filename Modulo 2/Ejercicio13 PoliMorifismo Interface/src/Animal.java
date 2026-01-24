053a7ee00ae27d817771593589c708b8085f63e0
patricioyaneza <pa.yaneza@outlook.cl>
2026-01-23 22:34:27 -0300

    UPDATE

---
+++ Modulo 2/Ejercicio13 PoliMorifismo Interface/src/Animal.java
@@ -0,0 +1,25 @@
+
+public class Animal implements IOperaciones {
+	public static void main(String[] args)
+	{
+		System.out.println("Hola");
+	}
+
+	@Override
+	public boolean guardar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void limpiar() {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ Modulo 2/Ejercicio13 PoliMorifismo Interface/src/IOperaciones.java
@@ -0,0 +1,10 @@
+
+public interface IOperaciones {
+	int IVA = 19;
+	Double PI = 3.14; 
+	
+	boolean guardar();
+	boolean eliminar();
+	void limpiar();
+	
+}
